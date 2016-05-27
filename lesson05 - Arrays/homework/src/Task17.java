b9e7b00c673917fbbdbcb4889266b76d2b40217c
BoyanEnchev <enchev.boyan@abv.bg>
2016-05-27 23:54:53 +0300

    changes

--- lesson05 - Arrays/homework/src/Task17.java
+++ lesson05 - Arrays/homework/src/Task17.java
@@ -3,7 +3,7 @@ public class Task17 {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 1 };
+		int[] arr = { 1, 3, 2, 4, 3, 7 };
 		boolean flag = true;
 
 		for (int i = 1; i < arr.length; i += 2) {
