929ea8a2bc0cbbdbee39140e98bea65ae5ef8d78
BoyanEnchev <enchev.boyan@abv.bg>
2016-06-19 20:03:41 +0300

    changes

--- Season5 - test1/src/MostFrequentWord.java
+++ Season5 - test1/src/MostFrequentWord.java
@@ -22,11 +22,36 @@ public class MostFrequentWord {
 		return words[index];
 	}
 
+	static String mostFrequentLetter(String text) {
+
+		text = text.replaceAll("[ ]", "");
+		String[] letters = text.split("");
+		int maxCount = 0, index = 0;
+
+		for (int index1 = 0; index1 < letters.length; index1++) {
+			int count = 0;
+
+			for (int index2 = index1; index2 < letters.length; index2++) {
+				if (letters[index1].equals(letters[index2])) {
+					count++;
+				}
+			}
+			if (count > maxCount) {
+				maxCount = count;
+				index = index1;
+			}
+		}
+
+		return letters[index];
+
+	}
+
 	public static void main(String[] args) {
 
-		String text = "cat horse dog horse dog lion tiger lion horse";
+		String text = "dog cat dog mouse pig dog cat dog";
 
-		System.out.println(mostFrequentWord(text));
+		System.out.println("word: " + mostFrequentWord(text));		//word: dog
+		System.out.println("letter: " + mostFrequentLetter(text));	//letter: o
 	}
 
 }
